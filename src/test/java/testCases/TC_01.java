936d7b5888b8b610a992636f19aa1198b7de3713
kuyr <kuyeemmanuel@rocketmail.com>
2019-12-04 20:00:11 +0100

    search username done

--- src/test/java/testCases/TC_01.java
+++ src/test/java/testCases/TC_01.java
@@ -2,35 +2,57 @@ package testCases;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TC_01 {
-    boolean isPresent;
-    @Test
+
+    //JSONArray array = new JSONArray();
+    @BeforeClass
+    public void setUp(){
+        RestAssured.baseURI="https://jsonplaceholder.typicode.com";
+
+    }
+
+    @Test(priority = 1)
     void getUsername()
     {
-        RestAssured.baseURI="https://jsonplaceholder.typicode.com";
         RequestSpecification httpRequest = RestAssured.given();
         Response response = httpRequest.request(Method.GET, "/users");
         String responseBody = response.getBody().asString();
-        //System.out.println("Response Body is:" + responseBody);
-        Assert.assertEquals(responseBody.contains("Samantha"),true);
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200);
-        JsonPath jsonpath = response.jsonPath();
-        var usernames = jsonpath.get("username");
-        int i;
-        int n = usernames.length;
-        for(i=0; i<n; i++){
-                if(usernames[i]=="Samantha"){
-                    System.out.println(i);
+        Assert.assertTrue(responseBody.contains("Samantha"));
+        JSONArray users = new JSONArray(responseBody);
+        int id = 0;
+        for(int i = 0; i<users.length(); i++){
+            JSONObject object = users.getJSONObject(i);
+            if("Samantha".equalsIgnoreCase(object.getString("username"))){
+                id = object.getInt("id");
+                System.out.println("username Samantha found with ID: "+id);
             }
+        }
+
+
+
+
+/////////////////////////////////////////////////////////////////////////
+//        var usernames = jsonpath.get("username");
+//        int i;
+//        int n = usernames.length;
+//        for(i=0; i<n; i++){
+//                if(jsonpath.[i]=="Samantha"){
+//                    System.out.println(i);
+           }
         }
         //System.out.println(usernames);
 
@@ -43,5 +65,5 @@ public class TC_01 {
 ///               System.out.println(singleObject.get("username"));
 //            }
 //        }
-    }
-}
+//    }
+//}
